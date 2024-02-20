7bcf88325eb1e9be18c94cc0f4c4f1ec29ffb5de
Brindha-sree <srikannan547@gmail.com>
2024-02-20 16:07:26 +0530

    back

--- index.js
+++ index.js
@@ -1,25 +1,67 @@
-const express =  require('express') 
-const dotenv = require ('dotenv');
-const connectDB = require( './config/db');
-const router = require("./routes/routes")
-const cors = require('cors')
-
-dotenv.config()
-
-connectDB();
-
+const express = require('express');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+const cors = require('cors');
+const fetch = require('node-fetch');
 const app = express();
+
+dotenv.config();
+connectDB();
 app.use(cors());
-app.use(express.json())
-app.use('/api', router)
-app.get('/', (req, res) =>{
-    res.send("<h1>Welcome to web scrapping</h1>")
-        
+
+const PORT = process.env.PORT || 4000;
+
+
+const username = 'BRINDHA'
+const password = 'Brindha@1988'
+
+
+app.get('/deals', async (req, res) => {
+  try {
+    const body = {
+      'source': 'amazon_product',
+      'domain': 'com',
+      'query': 'deals of the day',
+      'parse': true,
+      'pages': 1
+    };
+
+    const response = await fetch('https://realtime.oxylabs.io/v1/queries', {
+      method: 'post',
+      body: JSON.stringify(body),
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Basic ' + Buffer.from(`${username}:${password}`).toString('base64'),
+      }
+    });
+
+    const data = await response.json();
+    console.log('API Response:', data);
+
+    if (!data.results || data.results.length === 0) {
+      throw new Error('Unexpected API response format');
+    }
+
+     const results = data.results[0].content.results.organic;
     
-})
+     
+     const filteredDeals = results.filter(deal => deal.price_strikethrough);
 
-const PORT = process.env.PORT || 3030;
+    const sortedByBestDeal = filteredDeals.sort((b, a) =>
+     ((a.price_strikethrough - a.price_strikethrough) / a.price_strikethrough * 100) - 
+     ((b.price_strikethrough - b.price_strikethrough) / b.price_strikethrough * 100));
+    
 
-app.listen(PORT,() =>{
-    console.log(`server Running  on ${PORT}`);
-})
+    res.send(sortedByBestDeal);
+    
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+app.listen(PORT, () =>
+  console.log(`server Running on ${PORT}`)
+);
+
+ 
